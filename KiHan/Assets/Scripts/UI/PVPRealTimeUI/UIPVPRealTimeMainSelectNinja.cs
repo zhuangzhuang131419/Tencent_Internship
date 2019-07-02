97602ff049feebe1ea51b96845665e90b3a349df
Chicheng <chicheng.zheng@mail.mcgill.ca>
2019-07-02 14:07:55 +0800

    所有带战斗力的显示通灵兽名字

--- KiHan/Assets/Scripts/UI/PVPRealTimeUI/UIPVPRealTimeMainSelectNinja.cs
+++ KiHan/Assets/Scripts/UI/PVPRealTimeUI/UIPVPRealTimeMainSelectNinja.cs
@@ -7,29 +7,77 @@ using kihan_general_table;
 using KH.Ninja;
 using KH.Plugins;
 using GameJoyAPI;
-using naruto.protocol;
 using KH.Lua;
 
-public class UIPVPRealTimeMainSelectNinja : MonoBehaviour 
+public class UIPVPRealTimeMainSelectNinja : MonoBehaviour
 {
     /* 决斗场3.0 新增内容 */
 
     // 名字修改
     private static PvpFightType currentFightType;
+    private static int currentSystemID;
 
-    public static PvpFightType FightType
+    // 不同UI界面是否带有战力
+    private static Dictionary<string, bool> UIWindows = new Dictionary<string, bool>();
+
+    /// <summary>
+    /// 对外的接口
+    /// </summary>
+    public static void AddUIWindow(string windowTitle, bool isWithFight)
+    {
+        if (!UIWindows.ContainsKey(windowTitle))
+        {
+            UIWindows.Add(windowTitle, isWithFight);
+        }
+    }
+
+    public static int CurrentSysID
     {
         get
         {
-            if (KHUIManager.Instance.IsWindowVisible(UIDef.LEVEL_SELECTBG_UI))  // 通灵兽
+            return currentSystemID;
+        }
+        set
+        {
+            currentSystemID = value;
+            Debug.LogWarning("当前systemID");
+        }
+    }
+
+    public static bool IsWithFight
+    {
+        get
+        {
+
+            AddUIWindow(UIDef.PVP_REALTIME_MAIN, true);         // 忍术对决
+            AddUIWindow("UILua/Arena/ArenaNinjaSelect", true);  // 段位赛
+
+
+            foreach (string title in UIWindows.Keys)
             {
-                return PvpFightType.PvpFightType_Arena;
+                if (KHUIManager.Instance.IsWindowVisible(title))
+                {
+                    return UIWindows[title];
+                }
             }
-            else
+
+            if (currentSystemID == SystemConfigDef.Anbu_PVP_1v1 // 117 
+                || currentSystemID == SystemConfigDef.Challenge // 112
+                || currentSystemID == SystemConfigDef.PVP_REALTIME // 57
+                || currentSystemID == SystemConfigDef.Arena_Main // 105
+                || currentSystemID == SystemConfigDef.Championship // 108
+                || currentSystemID == SystemConfigDef.Psychic_1 // 51
+                || currentSystemID == 128
+                || currentSystemID == 255
+                || currentSystemID == 307)
             {
-                Debug.LogWarning("没有对应的");
-                return PvpFightType.PvpFightType_Count;
+                Debug.LogWarning("带有通灵兽");
+                return true;
             }
+
+
+            Debug.LogWarning("false");
+            return false;
         }
     }
 
@@ -38,40 +86,40 @@ public class UIPVPRealTimeMainSelectNinja : MonoBehaviour
     // 仅查看功能
 
     public static bool OnlyWatch = false;
-	public UIToggle ToggleOnlyWatch = null;
+    public UIToggle ToggleOnlyWatch = null;
 
-	public void OnClickOnlyWatch()
-	{
-		OnlyWatch = ToggleOnlyWatch.value;
-	}
+    public void OnClickOnlyWatch()
+    {
+        OnlyWatch = ToggleOnlyWatch.value;
+    }
 
-	public static int SPageNum = -1;
-	public static int APageNum = -1;
-	public static int BPageNum = -1;
-	public static int CPageNum = -1;
+    public static int SPageNum = -1;
+    public static int APageNum = -1;
+    public static int BPageNum = -1;
+    public static int CPageNum = -1;
 
-	public UILabel LblNinjaLevel = null;
-	public GameObject ObjNinajaLevelPopWindow = null;
-	public UIScrollView ScrollView;
-	public Vector3 RawPos;
-	public int GridWidth;
-	
-	public LuaBehaviour PageGroup;
-	public LuaBehaviourWrapper PageGroupNew;
+    public UILabel LblNinjaLevel = null;
+    public GameObject ObjNinajaLevelPopWindow = null;
+    public UIScrollView ScrollView;
+    public Vector3 RawPos;
+    public int GridWidth;
+
+    public LuaBehaviour PageGroup;
+    public LuaBehaviourWrapper PageGroupNew;
     public UISetTeamFilterItem teamFileterComp;
 
     private bool isRefreshFilterTab = false;
     public void OnEnable()
-	{
-		UIPVPRealTimeMainSelectNinja.OnlyWatch = false;
-		ToggleOnlyWatch.value = false;
-	}
+    {
+        UIPVPRealTimeMainSelectNinja.OnlyWatch = false;
+        ToggleOnlyWatch.value = false;
+    }
 
-	public void OnDisable()
-	{
-		UIPVPRealTimeMainSelectNinja.OnlyWatch = false;
-		ToggleOnlyWatch.value = false;
-	}
+    public void OnDisable()
+    {
+        UIPVPRealTimeMainSelectNinja.OnlyWatch = false;
+        ToggleOnlyWatch.value = false;
+    }
 
     public void RefreshFilterTab(bool isForceRefresh = true)
     {
@@ -108,8 +156,8 @@ public class UIPVPRealTimeMainSelectNinja : MonoBehaviour
         LblNinjaLevel.text = titleText;
     }
 
-	public void OnClickNinjaLevel()
-	{
+    public void OnClickNinjaLevel()
+    {
         if (NGUITools.GetActive(ObjNinajaLevelPopWindow))
         {
             NGUITools.SetActive(ObjNinajaLevelPopWindow, false);
@@ -121,55 +169,55 @@ public class UIPVPRealTimeMainSelectNinja : MonoBehaviour
         }
     }
 
-	public void OnClickNinjaLevelS()
-	{
-		SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (SPageNum - 1), 13f);
-		if(PageGroup != null)
-			PageGroup.CallLuaFunctionForLua("SetCurrentPage", SPageNum);
-		if(PageGroupNew != null)
-			PageGroupNew.CallLuaFunction("SetCurrentPage", SPageNum);
+    public void OnClickNinjaLevelS()
+    {
+        SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (SPageNum - 1), 13f);
+        if (PageGroup != null)
+            PageGroup.CallLuaFunctionForLua("SetCurrentPage", SPageNum);
+        if (PageGroupNew != null)
+            PageGroupNew.CallLuaFunction("SetCurrentPage", SPageNum);
 
-		ObjNinajaLevelPopWindow.SetActive(false);
+        ObjNinajaLevelPopWindow.SetActive(false);
 
-		LblNinjaLevel.text = "[ffcc00][u]S级忍者";
-	}
-	
-	public void OnClickNinjaLevelA()
-	{
-		SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (APageNum - 1), 13f);
-		if(PageGroup != null)
-			PageGroup.CallLuaFunctionForLua("SetCurrentPage", APageNum);
-		if(PageGroupNew != null)
-			PageGroupNew.CallLuaFunction("SetCurrentPage", APageNum);
+        LblNinjaLevel.text = "[ffcc00][u]S级忍者";
+    }
 
-		ObjNinajaLevelPopWindow.SetActive(false);
+    public void OnClickNinjaLevelA()
+    {
+        SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (APageNum - 1), 13f);
+        if (PageGroup != null)
+            PageGroup.CallLuaFunctionForLua("SetCurrentPage", APageNum);
+        if (PageGroupNew != null)
+            PageGroupNew.CallLuaFunction("SetCurrentPage", APageNum);
 
-		LblNinjaLevel.text = "[ffcc00][u]A级忍者";
-	}
-	
-	public void OnClickNinjaLevelB()
-	{
-		SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (BPageNum - 1), 13f);
-		if(PageGroup != null)
-			PageGroup.CallLuaFunctionForLua("SetCurrentPage", BPageNum);
-		if(PageGroupNew != null)
-			PageGroupNew.CallLuaFunction("SetCurrentPage", BPageNum);
+        ObjNinajaLevelPopWindow.SetActive(false);
 
-		ObjNinajaLevelPopWindow.SetActive(false);
+        LblNinjaLevel.text = "[ffcc00][u]A级忍者";
+    }
 
-		LblNinjaLevel.text = "[ffcc00][u]B级忍者";
-	}
-	
-	public void OnClickNinjaLevelC()
-	{
-		SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (CPageNum - 1), 13f);
-		if(PageGroup != null)
-			PageGroup.CallLuaFunctionForLua("SetCurrentPage", CPageNum);
-		if(PageGroupNew != null)
-			PageGroupNew.CallLuaFunction("SetCurrentPage", CPageNum);
+    public void OnClickNinjaLevelB()
+    {
+        SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (BPageNum - 1), 13f);
+        if (PageGroup != null)
+            PageGroup.CallLuaFunctionForLua("SetCurrentPage", BPageNum);
+        if (PageGroupNew != null)
+            PageGroupNew.CallLuaFunction("SetCurrentPage", BPageNum);
 
-		ObjNinajaLevelPopWindow.SetActive(false);
+        ObjNinajaLevelPopWindow.SetActive(false);
 
-		LblNinjaLevel.text = "[ffcc00][u]C级忍者";
-	}
+        LblNinjaLevel.text = "[ffcc00][u]B级忍者";
+    }
+
+    public void OnClickNinjaLevelC()
+    {
+        SpringPanel.Begin(ScrollView.gameObject, RawPos + Vector3.left * GridWidth * (CPageNum - 1), 13f);
+        if (PageGroup != null)
+            PageGroup.CallLuaFunctionForLua("SetCurrentPage", CPageNum);
+        if (PageGroupNew != null)
+            PageGroupNew.CallLuaFunction("SetCurrentPage", CPageNum);
+
+        ObjNinajaLevelPopWindow.SetActive(false);
+
+        LblNinjaLevel.text = "[ffcc00][u]C级忍者";
+    }
 }
