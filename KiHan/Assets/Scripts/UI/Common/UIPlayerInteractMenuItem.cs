7adb167d95634026a779b818a0996674dac20b3b
Chicheng <chicheng.zheng@mail.mcgill.ca>
2019-07-24 23:13:05 +0800

    add UIPlayerInteractMenuItem

---
+++ KiHan/Assets/Scripts/UI/Common/UIPlayerInteractMenuItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KH
+{
+    // 菜单项
+    public class UIPlayerInteractMenuItem : MonoBehaviour
+    {
+        public UILabel _Name;
+
+        private PlayerInteractInfo TargetPlayer;
+        private UIPlayerInteractPopup.MenuItemArg MenuItemArg;
+
+        public void Rebind(PlayerInteractInfo target, UIPlayerInteractPopup.MenuItemArg arg)
+        {
+            TargetPlayer = target;
+            MenuItemArg = arg;
+            _Name.text = arg.Name;
+        }
+
+        public void OnClick()
+        {
+            if (MenuItemArg == null) return;
+            // 触发菜单功能
+            MenuItemArg.OnClick(TargetPlayer);
+        }
+
+    }
+}
